19f580d54cd321cb9c58befdd6627425c5925129
Amir-Shawkat <amirshawkat05@gmail.com>
2022-02-07 00:46:20 +0600

    first commit

---
+++ Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    private float spawnz = 40f;
+    public GameObject[] Tile;
+    private int TileAmount=5;
+    private float dist = 3f;
+
+    private List<GameObject> Tilelists;
+    private Transform Tiletransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tilelists = new List<GameObject>();
+        Tiletransform = GameObject.FindGameObjectWithTag("Tile").transform;
+        for (int i = 0; i < TileAmount; ++i)
+            SpawnTile(0);
+    }
+
+    // Update is called once per frame
+
+    /* void Update()
+    {
+        if (Tiletransform.position.z < (spawnz - TileAmount * dist))
+        {
+            SpawnTile(0);
+            DeleteTile();
+        }   
+    }
+    void SpawnTile(int TileIndex )
+    {
+        GameObject go;
+
+        int randomTile = Random.Range(0, Tile.Length);
+        Vector3 hposition = new Vector3(Random.Range(-4f, 4f), 0.3f, spawnz);
+
+        go = Instantiate(Tile[randomTile], hposition, Tile[randomTile].transform.rotation) as GameObject;
+        spawnz += dist;
+
+        Tilelists.Add(go);
+    }
+    void DeleteTile()
+    {
+        Destroy(Tilelists[0]);
+        Tilelists.RemoveAt(0);
+    }*/
+    void Update()
+    {
+        if (spawnz<40f)
+        {
+            SpawnTile(0);
+            //spawnz += 10f;
+            DeleteTile();
+        }   
+    }
+    void SpawnTile(int TileIndex )
+    {
+        GameObject go;
+
+        int randomTile = Random.Range(0, Tile.Length);
+        Vector3 hposition = new Vector3(Random.Range(-4f, 4f), 0.3f, spawnz+10f);
+
+        go = Instantiate(Tile[randomTile], hposition, Tile[randomTile].transform.rotation) as GameObject;
+        // spawnz =go.transform.rotation.z;
+        //spawnz -= dist;
+
+        Tilelists.Add(go);
+    }
+    void DeleteTile()
+    {
+        Destroy(Tilelists[0]);
+        Tilelists.RemoveAt(0);
+    }
+}
---
+++ Assets/Scripts/TileMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMovement : MonoBehaviour
+{
+    private float zposition;
+    public GameObject Tile;
+    public GameObject Ball;
+    public GameObject Destroyer;
+    public Vector3 move;
+    public float  movespeed =5f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startRunning();
+    }
+
+    public void startRunning()
+    {
+        move = Vector3.zero;
+        move.z = -1 * movespeed * Time.deltaTime;
+        Tile.transform.Translate(0, 0, move.z);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "Tile")
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
